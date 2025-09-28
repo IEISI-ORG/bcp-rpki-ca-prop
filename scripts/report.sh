6cb061ae3ea3bcb037c2f3bbb4213d5a7f3194fc
Terry Sweetser <tcs@ieisi.org>
2025-09-28 11:26:28 +1000

    add csv report

--- scripts/report.sh
+++ scripts/report.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 python3 rpki_error_analyzer.py | tee reports/rpki_errors_$(date +%Y%m%d).txt
+python3 rpki_error_analyzer.py   --csv  psql/rpki_errors_$(date +%Y%m%d).csv
 
 #
 # you may be in the wrong dir, try this from the root of the repo
